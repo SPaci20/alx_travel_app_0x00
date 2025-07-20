10a3f15fba9b94300492f9ea5033b1b6b816a034
SPaci20 <spaci20@gmail.com>
2025-07-20 16:59:02 +0200

    Update serializers.py with proper formatting and documentation

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Listing, Booking
 
+# Serializers for the listings app
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
